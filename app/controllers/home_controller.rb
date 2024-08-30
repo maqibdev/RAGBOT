e4f73620db91ff4596100e1ba90d60e885e1a739
maqibdev <maqib@Mians-MacBook-Air.local>
2024-08-30 04:16:46 +0100

    Removing the old logic

--- app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  before_action :authenticate_user!, only: [ :index ]
-
-  def index
-    @research_paper = ResearchPaper.new
-    @chat = current_user.chats.includes(queries: :response).order(created_at: :asc).last
-  end
-end
